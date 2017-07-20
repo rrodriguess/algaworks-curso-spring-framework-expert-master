7bedfa99166bc6c75659ab9c197ad3977bd6434d
Renato <rrodriguez@outlook.com.br>
2017-07-20 16:44:07 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso Spring Framework Expert da AlgaWorks
+
+Aqui é onde coloco as alterações que fiz para responder as dúvidas dos alunos.
---
+++ src/main/java/com/algaworks/brewer/controller/SegurancaController.java
@@ -0,0 +1,29 @@
+package com.algaworks.brewer.controller;
+
+import com.algaworks.brewer.repository.Usuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SegurancaController {
+
+	@Autowired
+	private Usuarios usuario;
+
+	@GetMapping("/login")
+	public String login(@AuthenticationPrincipal User user) {
+		if (user != null) {
+			return "redirect:/cervejas";
+		}
+		return "Login";
+	}
+	
+	@GetMapping("/403")
+	public String acessoNegado() {
+		return "403";
+	}
+	
+}
